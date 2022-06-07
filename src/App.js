1542c1d0b2647e489537e78d26898623ca3b425c
Peter Ince <pdince@gmail.com>
2022-06-07 22:48:42 +1000

    add base template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import {PeraWalletConnect} from '@perawallet/connect';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import { useEffect, useState } from 'react';
+
+// Create the PeraWalletConnect instance outside the component
+const peraWallet = new PeraWalletConnect();
 
 function App() {
+  const [accountAddress, setAccountAddress] = useState(null);
+  const isConnectedToPeraWallet = !!accountAddress;
+
+  useEffect(() => {
+    // reconnect to session when the component is mounted
+    peraWallet.reconnectSession().then((accounts) => {
+      // Setup disconnect event listener
+      peraWallet.connector?.on('disconnect', handleDisconnectWalletClick);
+
+      if (accounts.length) {
+        setAccountAddress(accounts[0]);
+      }
+    })
+  },[]);
+  
   return (
-    <div className="App">
+    <Container>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button
+      onClick={
+        isConnectedToPeraWallet ? handleDisconnectWalletClick : handleConnectWalletClick
+      }>
+      {isConnectedToPeraWallet ? "Disconnect" : "Connect to Pera Wallet"}
+    </Button>
+          AlgoHUB - Lab 2
       </header>
-    </div>
+    </Container>
   );
+
+  function handleConnectWalletClick() {
+    peraWallet.connect().then((newAccounts) => {
+      // setup the disconnect event listener
+      peraWallet.connector?.on('disconnect', handleDisconnectWalletClick);
+
+      setAccountAddress(newAccounts[0]);
+    });
+  }
+
+    function handleDisconnectWalletClick() {
+      peraWallet.disconnect();
+      setAccountAddress(null);
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import the bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
